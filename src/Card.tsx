909ce94da28f0e823130d64f7f076feb9d656fbd
FrontRowWithJ <adebusum@tcd.ie>
2022-08-24 00:55:06 +0100

    Better organised code

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,8 @@
 import { IWorkout } from "./workoutData";
 import "./card.css";
-import { db_url, useSwitch } from "./util";
+import { db_url } from "./util";
 import { CSSProperties, useState } from "react";
+import Workout from "./Workout";
 
 interface CardProp {
   routine: readonly IWorkout[];
@@ -21,18 +22,10 @@ const Card = ({
   cardRef,
 }: CardProp) => {
   const [indicatorStyle, setIndicatorStyle] = useState<CSSProperties>();
-  const [opacities, setOpacities] = useSwitch(routine.length, 0);
-  const [tops, setTop] = useSwitch(routine.length, "-50%");
-  const [canPress, setPress] = useSwitch(routine.length, true);
-  const increase = (data: number[], i: number) => {
-    data[i]++;
-    setData([...data]);
-  };
-  const decrease = (data: number[], i: number) => {
-    data[i] = data[i] + (data[i] > 0 ? -1 : 0);
-    setData([...data]);
-  };
-
+  const increase = (data: number[], i: number) =>
+    setData(data.map((n, _i) => (i === _i ? n + 1 : n)));
+  const decrease = (data: number[], i: number) =>
+    data[i] > 0 && setData(data.map((n, _i) => (i === _i ? n - 1 : n)));
   return (
     <div ref={cardRef} className="card-container noselect" style={style}>
       <header className="noselect">{muscleGroup}</header>
@@ -41,54 +34,16 @@ const Card = ({
           <div className="scroller">
             {routine.map(({ workoutName, numOfSets, numOfReps }, i) => {
               return (
-                <div className="workout-row" key={`${workoutName}${i}`}>
-                  <div
-                    className="workout-label"
-                    style={{ cursor: canPress[i] ? "pointer" : "not-allowed" }}
-                    onPointerDown={() => {
-                      if (canPress[i]) {
-                        setPress(i, false);
-                        setOpacities(i, 1);
-                        setTop(i, "133.333%");
-                        setTimeout(() => {
-                          setPress(i, true);
-                          setOpacities(i, 0);
-                          setTop(i, "-100%");
-                        }, 1500);
-                      }
-                    }}
-                  >
-                    {workoutName}
-                    <div
-                      className="set-info noselect"
-                      style={{ opacity: opacities[i], top: tops[i] }}
-                    >
-                      Sets: {numOfSets}
-                      <br />
-                      Reps: {numOfReps}
-                      <div className="triangle"></div>
-                    </div>
-                  </div>
-                  {data && (
-                    <div className="input-container">
-                      <div
-                        onContextMenu={(e) => e.preventDefault()}
-                        draggable={false}
-                        onPointerDown={() => increase(data, i)}
-                        className="increase-button"
-                      >
-                        +
-                      </div>
-                      <div className="weight-label">{data[i]}</div>
-                      <div
-                        onPointerDown={() => decrease(data, i)}
-                        className="decrease-button"
-                      >
-                        -
-                      </div>
-                    </div>
-                  )}
-                </div>
+                data && (
+                  <Workout
+                    workoutName={workoutName}
+                    numOfSets={numOfSets}
+                    numOfReps={numOfReps}
+                    level={data[i]}
+                    increase={() => increase(data, i)}
+                    decrease={() => decrease(data, i)}
+                  />
+                )
               );
             })}
           </div>
@@ -96,13 +51,16 @@ const Card = ({
         <div
           className="save-button noselect"
           onPointerDown={() => {
-            setIndicatorStyle({ opacity: 1, bottom: "4rem" });
-            setTimeout(() => setIndicatorStyle({}), 1000);
             fetch(`${db_url}?${new URLSearchParams({ muscleGroup })}`, {
               method: "POST",
               mode: "cors",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify(data),
+            }).then(({ status }) => {
+              if (status === 200) {
+                setIndicatorStyle({ opacity: 1, bottom: "4rem" });
+                setTimeout(() => setIndicatorStyle({}), 1000);
+              }
             });
           }}
         >
--- src/util.ts
+++ src/util.ts
@@ -1,24 +1,9 @@
-import { useState } from "react";
 type Callback<T> = (value: number) => T;
 
 export const times = <T>(n: number, iter: Callback<T>) =>
   [...new Array(n).keys()].map<T>(iter);
 
 export const db_url = "https://gym-routine-backend.herokuapp.com/";
-
-type ISwitch = <T>(
-  length: number,
-  def: T
-) => [T[], (index: number, val: T) => void];
-
-export const useSwitch: ISwitch = <T>(length: number, def: T) => {
-  const [arr, setArr] = useState<T[]>(times(length, () => def));
-  const set = (index: number, val: T) => {
-    arr[index] = val;
-    setArr([...arr]);
-  };
-  return [arr, set];
-};
 
 export const translate = (e: HTMLDivElement, d: number) =>
   e && (e.style.left = d + "px");
