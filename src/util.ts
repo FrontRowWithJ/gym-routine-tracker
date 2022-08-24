99cc3abb424b47e64c6399a6d9dc6e206d16827b
FrontRowWithJ <adebusum@tcd.ie>
2022-08-24 23:50:49 +0100

    renamed variable

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@ type Callback<T> = (value: number) => T;
 export const times = <T>(n: number, iter: Callback<T>) =>
   [...new Array(n).keys()].map<T>(iter);
 
-export const db_url = "https://gym-routine-backend.herokuapp.com/";
+export const backend_uri = "https://gym-routine-backend.herokuapp.com/";
 
 export const translate = (e: HTMLDivElement, d: number) =>
   e && (e.style.left = d + "px");
