5b34a262c7c55419c62c6aa5c949b304827f191d
FrontRowWithJ <adebusum@tcd.ie>
2022-08-27 17:32:37 +0100

    Added comment for local development

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,7 @@ type Callback<T> = (value: number) => T;
 export const times = <T>(n: number, iter: Callback<T>) =>
   [...new Array(n).keys()].map<T>(iter);
 
+// const baseURL = "http://localhost:8888"
 const baseURL = "https://gym-tracker-db.netlify.app";
 export const getGymDataURL = `${baseURL}/.netlify/functions/get_gym_routine`;
 export const updateGymDataURL = `${baseURL}/.netlify/functions/update_gym_routine`;
