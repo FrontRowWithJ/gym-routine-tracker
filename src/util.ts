dceb2c5c24397100808ebf255112f182317ddb6c
FrontRowWithJ <adebusum@tcd.ie>
2022-08-27 05:36:00 +0100

    switched base url to deployed url

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,7 @@ type Callback<T> = (value: number) => T;
 export const times = <T>(n: number, iter: Callback<T>) =>
   [...new Array(n).keys()].map<T>(iter);
 
-const baseURL = "https://630915ef220a210304d38336--gym-tracker-db.netlify.app";
+const baseURL = "https://gym-tracker-db.netlify.app";
 export const getGymDataURL = `${baseURL}/.netlify/functions/get_gym_routine`;
 export const updateGymDataURL = `${baseURL}/.netlify/functions/update_gym_routine`;
 export const translate = (e: HTMLDivElement, d: number) =>
