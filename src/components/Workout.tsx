c06412a5031ccd91be123eab8bddca2289ec1108
FrontRowWithJ <adebusum@tcd.ie>
2022-08-28 19:28:03 +0100

    Moved files for better file organisation

---
+++ src/components/Workout.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "../style/workout.css";
+
+interface WorkoutProps {
+  workoutName: string;
+  numOfSets: number;
+  numOfReps: number;
+  level: number | undefined;
+  increase: () => void;
+  decrease: () => void;
+}
+
+const Workout = ({
+  workoutName,
+  numOfReps,
+  numOfSets,
+  level,
+  increase,
+  decrease,
+}: WorkoutProps) => {
+  const [canPress, setPress] = useState<boolean>(true);
+  const [opacity, setOpacity] = useState<number>(0);
+  const [top, setTop] = useState<string>("-100%");
+  return (
+    <div className="workout-row">
+      <div
+        className="workout-label"
+        style={{ cursor: canPress ? "pointer" : "not-allowed" }}
+        onClick={() => {
+          if (canPress) {
+            setPress(false);
+            setOpacity(1);
+            setTop("133.333%");
+            setTimeout(() => {
+              setPress(true);
+              setOpacity(0);
+              setTop("-100%");
+            }, 1500);
+          }
+        }}
+      >
+        {workoutName}
+        <div className="set-info noselect" style={{ opacity, top }}>
+          Sets: {numOfSets}
+          <br />
+          Reps: {numOfReps}
+          <div className="triangle"></div>
+        </div>
+      </div>
+      {
+        <div className="input-container">
+          <div
+            draggable={false}
+            onPointerDown={() => increase()}
+            className="increase-button"
+          >
+            +
+          </div>
+          {level && <div className="weight-label">{level}</div>}
+          <div onPointerDown={() => decrease()} className="decrease-button">
+            -
+          </div>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Workout;
