db48f72f0081c1818f20ef514a4739d0fd2efd6c
FrontRowWithJ <adebusum@tcd.ie>
2023-05-03 19:34:38 +0100

    removed unused contexts

--- src/misc/contexts.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-import { screenRoute } from "./types";
-
-export const PathContext = createContext<{
-  path?: screenRoute;
-  setPath?: React.Dispatch<React.SetStateAction<screenRoute>>;
-}>({});
