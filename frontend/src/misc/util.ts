96b5f43f85f9b5963b95b9f78f703793faf4903f
FrontRowWithJ <adebusum@tcd.ie>
2023-08-06 22:52:32 +0100

    Functions path is more dynamic

--- frontend/src/misc/util.ts
+++ frontend/src/misc/util.ts
@@ -7,8 +7,10 @@ export function times<T>(n: number, iter: T | Callback<T>) {
   const cb = typeof iter === "function" ? iter : () => iter;
   return [...new Array(n).keys()].map(cb as Callback<T>);
 }
+export const FUNCTIONS_PATH = `${
+  process.env["NODE_ENV"] === "DEVELOPMENT" ? "http://localhost:8888" : ""
+}/.netlify/functions`;
 
-export const FUNCTIONS_PATH = "/.netlify/functions";
 export const translate = (e: HTMLDivElement, d: number) =>
   e && (e.style.left = d + "px");
 export const getLeft = (i: number, x: number) => (i - x) * 100 + "%";
